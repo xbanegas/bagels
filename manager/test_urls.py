e180ad5e1438a3b4fa5b96b8184898e1c0813a2a
Xavier Banegas <xbanegas@gmail.com>
2017-08-02 21:52:05 -0500

    move api tests, bookmark form accordion

--- manager/test_urls.py
+++ manager/test_urls.py
@@ -14,38 +14,8 @@ class TestManagerUrls(TestCase):
     def test_list_resolve(self):
         self.assertEqual(resolve('/').view_name, 'manager:home')
 
-    # @TODO move api to own namespace
+    def test_list_by_tag_reverse(self):
+        self.assertEqual(reverse('manager:list_by_tag', args=[23]), '/list/23/')
 
-
-    def test_api_bookmarks_reverse(self):
-        self.assertEqual(reverse('api:link_rest_api'), '/api/bookmarks/')
-
-    def test_api_bookmarks_resolve(self):
-        self.assertEqual(
-            resolve('/api/bookmarks/').view_name, 'api:link_rest_api'
-        )
-
-    def test_api_bookmarks_detail_reverse(self):
-        self.assertEqual(
-            reverse('api:link_rest_api_detail', kwargs={ 'id':1 }),
-            '/api/bookmarks/1/'
-        )
-
-    def test_api_tags_reverse(self):
-        self.assertEqual(reverse('api:tag_rest_api'), '/api/tags/')
-
-    def test_api_tags_resolve(self):
-        self.assertEqual(
-            resolve('/api/tags/').view_name, 'api:tag_rest_api'
-        )
-
-    def test_api_tags_detail_reverse(self):
-        self.assertEqual(
-            reverse('api:tag_rest_api_detail', kwargs={'id':1}),
-            '/api/tags/1/'
-        )
-
-    def test_api_tags_detail_resolve(self):
-        self.assertEqual(
-            resolve('/api/tags/1/').view_name, 'api:tag_rest_api_detail'
-        )
+    def test_list_by_tag_resolve(self):
+        self.assertEqual(resolve('/list/23/').view_name,'manager:list_by_tag')
